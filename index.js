068a2da807434c804b77230e2468eccf326921fc
Akshay K Pillai <akshaykpillai369@gmail.com>
2026-09-23 10:02:42 +0530

    Implement HTTP server for HackerBot status
    Added an HTTP server to respond with a status message.

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const { App } = require('@slack/bolt');
 const axios = require('axios');
 
+const port = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('HackerBot is awake and running!\n');
+}).listen(port, () => {
+  console.log(`HTTP ping server listening on port ${port}`);
+});
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
